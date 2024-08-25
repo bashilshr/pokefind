fe0743cae51ab660b4ec1fbaf058b830eea77fa0
bashilshr <120891175+bashilshr@users.noreply.github.com>
2024-08-25 20:17:02 +0545

    chore: minor fix import statements in pokemon.py

--- app/routers/pokemon.py
+++ app/routers/pokemon.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from ..database import get_db
-from ..models import Pokemon
+from database import get_db
+from models import Pokemon
 
 router = APIRouter()
 
